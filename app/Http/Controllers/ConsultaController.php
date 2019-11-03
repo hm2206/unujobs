fe7c4309f6030b792f311f3ea43c2689e463dbda
Hans Medina <twd2206@gmail.com>
2019-11-02 23:14:35 -0500

    ready

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -43,7 +43,7 @@ class ConsultaController extends Controller
         $historial = Historial::where('work_id', $work->id)
             ->whereHas('cronograma', function($cro) use($year) {
                 $cro->where('año', $year);
-            })->get();
+            })->where('pdf', "<>", null)->get();
 
         return view('consultas.work', compact('work', 'meses', 'year', 'historial'));
     }
